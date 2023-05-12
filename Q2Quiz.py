9e8f162beb5f40e55a02d09aabe1f326cfa4ba0a
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-05-12 10:19:37 -0400

    Update Q2Quiz.py

--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,16 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+# Open the file in read mode
+with open("questions.txt", "r") as file:
+    score = 0
+    for line in file:
+        if line.startswith("Correct Answer"):
+            user_answer = input("Your answer: ")
+            if user_answer.upper() == correct_answer:
+                score += 1
+        else:
+            print(line.rstrip())
+            if line.startswith(("A.", "B.", "C.", "D.")):
+                if line.startswith("Correct Answer"):
+                    correct_answer = line[-2]
+    print(f"You scored {score} out of 5.")
